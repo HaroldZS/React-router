a874b80455a511e3a41c84abd3192591c0dd53f4
haroldZS <haroldzs1024@gmail.com>
2023-04-15 14:34:45 -0400

    React router blogpost navigation

---
+++ src/blogdata.js
@@ -0,0 +1,21 @@
+const blogdata = [];
+blogdata.push({
+  title: "¿Qué es React?",
+  slug: "que-es-react",
+  content: "React es el mejor framework de Javascript",
+  author: "Haroldzs",
+});
+blogdata.push({
+  title: "¿Qué es Vue?",
+  slug: "que-es-vue",
+  content: "Vue es el mejor framework de Javascript",
+  author: "Haroldzs",
+});
+blogdata.push({
+  title: "¿Qué es Angular?",
+  slug: "que-es-angular",
+  content: "Angular es el mejor framework de Javascript",
+  author: "Haroldzs",
+});
+
+export { blogdata };
