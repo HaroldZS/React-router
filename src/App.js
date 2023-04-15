9d718c54ff8e4988eca079af723455acd1ccf077
haroldZS <haroldzs1024@gmail.com>
2023-04-15 18:45:17 -0400

    Authentication and Protected Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { Menu } from "./Menu";
+import { AuthProvider, AuthRoute } from "./auth";
 import { HomePage } from "./HomePage";
 import { BlogPage } from "./BlogPage";
 import { BlogPost } from "./BlogPost";
 import { ProfilePage } from "./ProfilePage";
+import { LoginPage } from "./LoginPage";
+import { LogoutPage } from "./LogoutPage";
 
 function App() {
   return (
     <>
       <HashRouter>
-        <Menu />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/blog" element={<BlogPage />}>
-          <Route path=":slug" element={<BlogPost />} />
-          </Route>
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="*" element={<p>Not found</p>} />
-        </Routes>
+        <AuthProvider>
+          <Menu />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/blog" element={<BlogPage />}>
+              <Route path=":slug" element={<BlogPost />} />
+            </Route>
+
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/logout" element={<AuthRoute>
+              <LogoutPage />
+            </AuthRoute>} />
+            <Route path="/profile" element={
+            <AuthRoute>
+              <ProfilePage />
+            </AuthRoute>
+            } />
+            <Route path="*" element={<p>Not found</p>} />
+          </Routes>
+        </AuthProvider>
       </HashRouter>
     </>
   );
--- src/Menu.js
+++ src/Menu.js
@@ -1,53 +1,32 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import { useAuth } from "./auth";
 
 function Menu() {
+  const auth = useAuth();
+
   return (
     <nav>
       <ul>
-        {routes.map((route) => (
-          <li key={route.to}>
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? "red" : "blue",
-              })}
-              to={route.to}
-            >
-              {route.text}
-            </NavLink>
-          </li>
-        ))}
+        {routes.map((route) => {
 
-        {/* <li>
-            <Link to="/">Home</Link>
-        </li>
-        <li>
-            <Link to="/blog">Blog</Link>
-        </li>
-        <li>
-            <Link to="/Profile">profile</Link>
-        </li> */}
+          if (!auth.user && route.private) return null;
 
-        {/* <li>
-          <NavLink
-            style={({isActive}) => ({
-              color: isActive ? "red" : "blue",
-            })}
-            to="/"
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink style={({isActive}) => ({
-              color: isActive ? "red" : "blue",
-            })} to="/blog">Blog</NavLink>
-        </li>
-        <li>
-          <NavLink style={({isActive}) => ({
-              color: isActive ? "red" : "blue",
-            })} to="/Profile">profile</NavLink>
-        </li> */}
+          if (auth.user && route.text === "Login") return null;
+
+          return (
+            <li key={route.to}>
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? "red" : "blue",
+                })}
+                to={route.to}
+              >
+                {route.text}
+              </NavLink>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
@@ -58,14 +37,27 @@ const routes = [];
 routes.push({
   to: "/",
   text: "Home",
+  private: false,
 });
 routes.push({
   to: "/blog",
   text: "Blog",
+  private: false,
 });
 routes.push({
   to: "/profile",
   text: "Profile",
+  private: true,
+});
+routes.push({
+  to: "/login",
+  text: "Login",
+  private: false,
+});
+routes.push({
+  to: "/logout",
+  text: "Logout",
+  private: true,
 });
 
 export { Menu };
